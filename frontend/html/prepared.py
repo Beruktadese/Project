445e243f8b5ceb74634e4ba2b1e8114803a0631c
Beruktadese <buramelkam@gmail.com>
2025-02-04 01:16:33 +0300

    revised project

---
+++ frontend/html/prepared.py
@@ -0,0 +1,46 @@
+# l=list(("True",12,"man","False"))
+# l.pop()
+# print(l)
+
+# list=["true",True,12,23.4,"name"]
+# del list
+# print(list)
+# print(list)
+
+# def abc():
+#     a=["true",23,34.5,True,"false"]
+#     a.pop(3)
+#     a.insert(2,"women")
+#     a.append(234)
+#     print(a)
+# abc()
+
+# li=[123,23,34,4]
+# # li.sort(reverse=True)
+# b=sum(li)
+# print(b)
+
+# T=(12,44,5,90)   
+# b=sorted(T)
+# print(b)
+# m=list(t)
+# m.sort()
+# print(m)
+
+# dic={"hello":23,"people":"man"}
+# dic2={"tru":43,"mam":"true",}
+# dic2.update
+# print(dic.get("people"))
+
+# n=int(input("enter the value"))
+# data={}
+# for _ in range(n):
+#     key=input("enter the key")
+#     value=input("enter the value")
+#     data[key]=value
+# print(f"\nsum of value:{sum(data.value())}")    
+with open("gg.txt","r") as file:
+    print("initial position:",file.tell())
+    data=file.read(10)
+    print("")
+
